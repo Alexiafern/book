3ac546cf0b1dc4e48bdac6a97175cfff87061244
Alexia Fernanda <alexiafernanda718@gmail.com>
2023-08-27 13:06:22 -0300

    Add arquivos

---
+++ paginas/menu.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+
+  <head>
+    <title>Meu diario</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="./image/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../imagens/ashe.webp">
+    <link rel="icon" type="image/png" sizes="16x16" href="./image/favicons/favicon-16x16.png">
+    <link rel="manifest" href="./image/favicons/site.webmanifest">
+    <link rel="mask-icon" href="./image/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="../css/style.css">
+  </head>
+
+  <body>
+
+      <!-- início topo -->
+      <div class="topo conteudo-flex">
+
+        <div class="topo-esquerdo conteudo-flex">
+          <!-- início do logo -->
+          <div class="topo-logo">
+            <img src="../imagens/ashe.webp" alt="Imagem logotipo do diario" title="Imagem logotipo do diário">
+          </div>
+          <!-- fim do logo -->
+        </div>
+
+        <!-- início topo centro -->
+        <div class="topo-centro conteudo-flex">
+
+          <!-- início menu de navegação -->
+          <nav>
+            <ul class="topo-menu conteudo-flex">
+              <li><a href="index.php">Início</a></li>
+              <li><a href="livros/index.php">Livros</a></li>
+              <li><a href="filmes/index.php">Webtoons</a></li>
+              <li><a href="doramas/index.php">Mangá</a></li>
+              <li><a href="categorias.php">Categorias</a></li>
+            </ul>
+          </nav>
+          <!-- fim do menu de navegação -->
+
+          <!-- início barra de pesquisa -->
+          <!--<div class="topo-pesquisa">
+            <div class="conteudo-pesquisa">
+              <form class="conteudo-flex" method="post">
+              <fieldset class="bar-pesquisa">
+                <input type="text" placeholder="Pesquisar um filme ou série..." alt="Digite aqui um filme ou série para pesquisar">
+              </fieldset>
+              <fieldset class="btn-pesquisar conteudo-flex">
+                <button type="button" title="Pesquisar">
+                  <span class="fas fa-search"></span>
+                </button>
+              </fieldset>
+            </form>
+            </div>
+          </div>-->
+          <!-- fim da barra de pesquisa -->
+        </div>
+        <!-- fim topo centro -->
+
+        <!-- início topo direito -->
+       <!-- <div class="topo-direito">-->
+          <!-- área de login -->
+         <!--<div class="sublogin">
+            <div>
+              <button class="subloginbtn">Fazer Login<br>ou Assinar <span class="fa fa-caret-down"></span></button>
+            </div>
+            <div class="sublogin-conteudo">
+              <a href="login.php"><button type="button" title="Entrar">Entrar</span></button></a>
+              <p>ou</p>
+              <a href="cadastro_login.php"><button type="button" title="Assinar">Assinar</span></button></a>
+              <p>ou</p>
+              <a href="cadastrar.php"><button type="button" title="Cadastrar">Cadastrar</span></button></a>
+              
+            </div>
+          </div>--> 
+          <!-- fim área de login -->
+        </div>
+        <!-- fim topo direito -->
+
+    </div>
+    <!-- fim do topo -->
+</body>
+</html>
