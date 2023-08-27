f27e4e8750e8b11255fa86e619b23ca41bf30421
Alexia Fernanda <alexiafernanda718@gmail.com>
2023-08-27 19:20:40 -0300

    Add no index.php

--- paginas/livros/index.php
+++ paginas/livros/index.php
@@ -1,16 +1,61 @@
 <?php
-include("./menu/menu.php");
+include('./menu/menu.php');
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="./css/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Container de Livros</title>
 </head>
 <body>
+
+<div class="container">
+    <div class="livro">
+        <img src="./imagens/DreamShaper_v7_generate_an_image_for_a_fantasy_book_0.jpg" alt="Capa do Livro 1">
+        <h2>Título do Livro 1</h2>
+        <p>Autor: Autor do Livro 1</p>
+        <p>Gênero: Gênero do Livro 1</p>
+    </div>
     
+    <div class="livro">
+        <img src="./imagens/DreamShaper_v7_generate_an_image_for_a_fantasy_book_0.jpg" alt="Capa do Livro 2">
+        <h2>Título do Livro 2</h2>
+        <p>Autor: Autor do Livro 2</p>
+        <p>Gênero: Gênero do Livro 2</p>
+    </div>
     
+    <div class="livro">
+        <img src="./imagens/DreamShaper_v7_generate_an_image_for_a_fantasy_book_0.jpg" alt="Capa do Livro 3">
+        <h2>Título do Livro 3</h2>
+        <p>Autor: Autor do Livro 3</p>
+        <p>Gênero: Gênero do Livro 3</p>
+    </div>
+
+    <div class="livro">
+        <img src="./imagens/DreamShaper_v7_generate_an_image_for_a_fantasy_book_0.jpg" alt="Capa do Livro 3">
+        <h2>Título do Livro 3</h2>
+        <p>Autor: Autor do Livro 3</p>
+        <p>Gênero: Gênero do Livro 3</p>
+    </div>
+
+    <div class="livro">
+        <img src="./imagens/DreamShaper_v7_generate_an_image_for_a_fantasy_book_0.jpg" alt="Capa do Livro 3">
+        <h2>Título do Livro 3</h2>
+        <p>Autor: Autor do Livro 3</p>
+        <p>Gênero: Gênero do Livro 3</p>
+    </div>
+
+    <div class="livro">
+        <img src="./imagens/DreamShaper_v7_generate_an_image_for_a_fantasy_book_0.jpg" alt="Capa do Livro 3">
+        <h2>Título do Livro 3</h2>
+        <p>Autor: Autor do Livro 3</p>
+        <p>Gênero: Gênero do Livro 3</p>
+    </div>
+</div>
+
 </body>
 </html>
