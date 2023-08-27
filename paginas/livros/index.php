9df74707a490d6e9d3ee79361d2ee15bcf58f2bf
Alexia Fernanda <alexiafernanda718@gmail.com>
2023-08-27 18:50:54 -0300

    Modificando

--- paginas/livros/index.php
+++ paginas/livros/index.php
@@ -1,32 +1,16 @@
 <?php
-include("../menu.php");
+include("./menu/menu.php");
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Meu diario</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
 </head>
 <body>
-      <!-- início contéudo principal -->
-  <div class="conteudo-principal">
-
-    <div class="primeiro-h1">
-      <h1>Que tal também assistir uma destas Séries?</h1>
-    </div>
     
-    <!-- início banner -->
-    <div class="elementos conteudo-flex">
-      <div class="banner">
-        <a href="07-serie.html"><img src="../image/love_death_and_robots2.jpg" alt="modelo banner de love_death_and_robots" title="love_death_and_robots"></a>
-      </div>
-      <div class="banner">
-        <a href="08-serie.html"><img src="../image/sandman2.jpg" alt="modelo banner sandman série" title="modelo banner sandman série"></a>
-      </div>
-    </div>
-    <!-- fim banner -->
     
 </body>
 </html>
---
+++ paginas/livros/index_livros.php
@@ -0,0 +1,100 @@
+<?php
+include("./menu/menu.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Meu diario</title>
+</head>
+<body>
+
+    <!-- início contéudo principal -->
+  <div class="conteudo-principal">
+
+<div class="primeiro-h1">
+  <h1>Que tal também ver sobre esses temas?</h1>
+  <br>
+</div>
+
+<!-- início banner -->
+<div class="elementos conteudo-flex">
+  <div class="banner">
+    <a href="07-serie.html"><img src="../image/love_death_and_robots2.jpg" alt="modelo banner love_death_and_robots série" title="modelo banner love_death_and_robots série"></a>
+  </div>
+  <div class="banner">
+    <a href="09-serie.html"><img src="../image/sandman2.jpg" alt="modelo banner sandman série" title="modelo banner sandman série"></a>
+  </div>
+</div>
+<!-- fim banner -->
+
+<!-- início elemento -->
+<div class="categorias">
+  <h1>Fantasia</h1>
+</div>
+<div class="elementos conteudo-flex">
+  <div class="poster-play">
+    <div>
+      <h2>O Bárbaro dos Raios Divinos</h2>
+    </div>
+    <div>
+      <img src="imagens/DreamShaper_v7_generate_an_image_for_a_fantasy_book_0.jpg" alt="imagem modelo poster ela é o cara filme" title="imagem modelo poster ela é o cara filme">
+    </div>
+    <div>
+      <a href="../paginas/livros/o_barbaro_dos_raios_divinos.html"><button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Ler</button></a>
+    </div>
+  </div>
+
+  <!--<div class="poster-play">
+    <div>
+      <h2>Pequenos espiões</h2>
+    </div>
+    <div>
+      <img src="../image/pequenos_espioes.jpg" alt="imagem modelo poster pequenos_espioes filme" title="imagem modelo poster pequenos_espioes filme">
+    </div>
+    <div>
+      <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
+    </div>
+  </div>
+
+  <div class="poster-play">
+    <div>
+      <h2>Love death and robots</h2>
+    </div>
+    <div>
+      <img src="../image/love_death_and_robots.jpg" alt="imagem modelo poster love_death_and_robots filme" title="imagem modelo poster love_death_and_robots filme">
+    </div>
+    <div>
+      <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
+    </div>
+  </div>
+
+  <div class="poster-play">
+    <div>
+      <h2>O cravo e a rosa</h2>
+    </div>
+    <div>
+      <img src="../image/o_cravo_e_a_rosa_logo.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
+    </div>
+    <div>
+      <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
+    </div>
+  </div>
+
+  <div class="poster-play">
+    <div>
+      <h2>Carrossel</h2>
+    </div>
+    <div>
+      <img src="../image/carrossel_logo.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
+    </div>
+    <div>
+      <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
+    </div>-->
+  </div>
+</div>
+    
+</body>
+</html>
