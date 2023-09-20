e4b43da2636b6851672fa53192bbac38f8c1cd8c
Alexiafern <alexiafernanda718@gmail.com>
2023-09-20 08:11:33 -0300

    add

--- paginas/livros/categorias.php
+++ paginas/livros/categorias.php
@@ -1,327 +1,45 @@
-<?php
-include('./menu/menu.php');
-?>
 <!DOCTYPE html>
-<html lang="pt-br" dir="ltr">
-
+<html lang="pt-br">
 <head>
-  <title>Filmov - Categoria de Filmes</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width-device-width, initial-scale=1">
-  <link rel="apple-touch-icon" sizes="180x180" href="../image/favicons/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="../image/favicons/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="../image/favicons/favicon-16x16.png">
-  <link rel="manifest" href="../image/favicons/site.webmanifest">
-  <link rel="mask-icon" href="../image/favicons/safari-pinned-tab.svg" color="#5bbad5">
-  <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="theme-color" content="#ffffff">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-  <link rel="stylesheet" href="./css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/categorias.css">
+    <title>Categorias</title>
 </head>
-
 <body>
+    <main>
+        <div class="categoria-card">
+            <?php
+                // Array de categorias (você pode buscar isso de um banco de dados)
+                $categorias = array(
+                    "Eletrônicos",
+                    "Roupas",
+                    "Livros",
+                    "Alimentos",
+                    "Esportes"
+                );
+
+                // Loop para exibir as categorias
+                foreach ($categorias as $categoria) {
+                    echo "<div class='card'>$categoria</div>";
+                }
+            ?>
+        </div>
+        <script>
+          window.addEventListener('resize', function() {
+              const categoriaCard = document.querySelector('.categoria-card');
+              const cards = document.querySelectorAll('.card');
+              const cardWidth = cards[0].offsetWidth;
+              const windowWidth = window.innerWidth;
+              const numVisibleCards = Math.floor(windowWidth / cardWidth);
+              categoriaCard.style.transform = `translateX(0px)`;
+              categoriaCard.style.transition = 'transform 0.3s';
   
-  <!-- início contéudo principal -->
-  <div class="conteudo-principal">
-
-    <div class="primeiro-h1">
-      <h1>Que tal também ver sobre esses temas?</h1>
-      <br>
-    </div>
-
-    <!-- início banner -->
-    <div class="elementos conteudo-flex">
-      <div class="banner">
-        <a href="07-serie.html"><img src="../image/love_death_and_robots2.jpg" alt="modelo banner love_death_and_robots série" title="modelo banner love_death_and_robots série"></a>
-      </div>
-      <div class="banner">
-        <a href="09-serie.html"><img src="../image/sandman2.jpg" alt="modelo banner sandman série" title="modelo banner sandman série"></a>
-      </div>
-    </div>
-    <!-- fim banner -->
-
-    <!-- início elemento -->
-    <div class="categorias">
-      <h1>Fantasia</h1>
-    </div>
-    <div class="elementos conteudo-flex">
-      <div class="poster-play">
-        <div>
-          <h2>O Bárbaro dos Raios Divinos</h2>
-        </div>
-        <div>
-          <img src="./imagens/DreamShaper_v7_generate_an_image_for_a_fantasy_book_0.jpg" alt="imagem modelo poster ela é o cara filme" title="imagem modelo poster ela é o cara filme">
-        </div>
-        <div>
-          <a href="./o_barbaro_dos_raios_divinos.html"><button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Ler</button></a>
-        </div>
-      </div>
-
-      <!--<div class="poster-play">
-        <div>
-          <h2>Pequenos espiões</h2>
-        </div>
-        <div>
-          <img src="../image/pequenos_espioes.jpg" alt="imagem modelo poster pequenos_espioes filme" title="imagem modelo poster pequenos_espioes filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-
-      <div class="poster-play">
-        <div>
-          <h2>Love death and robots</h2>
-        </div>
-        <div>
-          <img src="../image/love_death_and_robots.jpg" alt="imagem modelo poster love_death_and_robots filme" title="imagem modelo poster love_death_and_robots filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-
-      <div class="poster-play">
-        <div>
-          <h2>O cravo e a rosa</h2>
-        </div>
-        <div>
-          <img src="../image/o_cravo_e_a_rosa_logo.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-
-      <div class="poster-play">
-        <div>
-          <h2>Carrossel</h2>
-        </div>
-        <div>
-          <img src="../image/carrossel_logo.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>-->
-      </div>
-    </div>
-    <!-- fim elemento -->
-
-    <!-- início elemento -->
-    <div class="categorias">
-      <h1>Drama</h1>
-    </div>
-    <div class="elementos conteudo-flex">
-      <div class="poster-play">
-        <h2>A cinco passos de você</h2>
-        <div>
-          <img src="../image/a_cinco_passos_de_voce.jpg" alt="imagem modelo poster a_cinco_passos_de_voce filme" title="imagem modelo poster a_cinco_passos_de_voce filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-<!--
-      <div class="poster-play">
-        <h2>Mulan</h2>
-        <div>
-          <img src="../image/mulan.jpg" alt="imagem modelo poster mulan filme" title="imagem modelo poster mulan filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-
-      <div class="poster-play">
-        <h2>Rebelde</h2>
-        <div>
-          <img src="../image/rebelde_logo.jg.jfif" alt="imagem modelo poster peaky_blinders serie" title="imagem modelo poster peaky_blinders series">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-
-      <div class="poster-play">
-        <h2>Teresa</h2>
-        <div>
-          <img src="../image/teresa_logo.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-
-      <div class="poster-play">
-        <h2>Poliana</h2>
-        <div>
-          <img src="../image/poliana_logo.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>-->
-    </div>
-    <!-- fim elemento -->
-
-    <!-- início elemento -->
-    <div class="categorias">
-      <h1>Suspense</h1>
-    </div>
-    <div class="elementos conteudo-flex">
-      <div class="poster-play">
-        <h2>Percy Jackson</h2>
-        <div>
-          <img src="../image/percy_jackson.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-      <!--
-
-      <div class="poster-play">
-        <h2>Peaky Blinders</h2>
-        <div>
-          <img src="../image/peaky_blinders.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-
-      <div class="poster-play">
-        <h2>Round 6</h2>
-        <div>
-          <img src="../image/round_6_logo.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-
-      <div class="poster-play">
-        <h2>Lucifer</h2>
-        <div>
-          <img src="../image/lucifer_logo.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-
-      <div class="poster-play">
-        <h2>O clone</h2>
-        <div>
-          <img src="../image/o_clone_logo.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>-->
-    </div>
-    <!-- fim elemento -->
-
-    <!-- início elemento -->
-    <div class="categorias">
-      <h1>Ficção</h1>
-    </div>
-    <div class="elementos conteudo-flex">
-      <div class="poster-play">
-        <h2>Pequenos Espiões</h2>
-        <div>
-          <img src="../image/pequenos_espioes.jpg" alt="imagem modelo poster pequenos_espioes filme" title="imagem modelo poster pequenos_espioes filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-      <!--
-
-      <div class="poster-play">
-        <h2>Love Death And Robots</h2>
-        <div>
-          <img src="../image/love_death_and_robots.jpg" alt="imagem modelo poster love_death_and_robots serie" title="imagem modelo poster love_death_and_robots serie">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-
-      <div class="poster-play">
-        <h2>Harry Potter</h2>
-        <div>
-          <img src="../image/harry_potter.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-
-      <div class="poster-play">
-        <h2>Percy Jackson</h2>
-        <div>
-          <img src="../image/percy_jackson.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>
-
-      <div class="poster-play">
-        <h2>winks</h2>
-        <div>
-          <img src="../image/winks_logo.jpg" alt="imagem modelo poster de filme" title="imagem modelo poster de filme">
-        </div>
-        <div>
-          <button type="button" id="btn-assistir" title="Assistir"><span class="fas fa-play"></span>Assistir</button>
-        </div>
-      </div>-->
-
-    </div>
-    <!-- fim elemento -->
-
-  </div>
-  <!-- fim conteúdo principal -->
-
-  <!-- início rodape -->
-  <footer>
-    <div class="site-map conteudo-flex">
-      <div class="site-map-conteudo">
-        <ul>
-          <li><a href="../index.html">Sobre nós</a></li>
-          <li><a href="#">Carreiras</a></li>
-          <li><a href="#">Perguntas frequentes</a></li>
-          <li><a href="#">Imprensa</a></li>
-        </ul>
-      </div>
-      <div class="site-map-conteudo">
-        <ul>
-          <li><a href="#">Política de Privacidade</a></li>
-          <li><a href="#">Política de Segurança</a></li>
-          <li><a href="#">Termo de Uso</a></li>
-          <li><a href="#">Avisos Legais</a></li>
-        </ul>
-      </div>
-      <div class="site-map-conteudo">
-        <ul>
-          <li><a href="#">Conta</a></li>
-          <li><a href="contato.html">Contato</a></li>
-          <li><a href="#">Centro de Ajuda</a></li>
-          <li><a href="#">Teste de Velocidade</a></li>
-        </ul>
-      </div>
-    </div>
-    <div>
-      <img src="../image/favicons/senac-logo.jpg" alt="Imagem logotipo da Filmlexi" title="Imagem logotipo da Filmlexi">
-    </div>
-    <div>
-      <p>2022 - Filmlexi S.A. by <a href="https://github.com/Alexiafern">Alexia Fernanda</a></p>
-      <p>Fotografias sob licença gratuita - fonte: <a href="http://www.pexels.com" target="_blank">pexels.com</a></p>
-    </div>
-  </footer>
-  <!-- fim rodape -->
-
+              if (numVisibleCards < cards.length) {
+                  categoriaCard.style.transform = `translateX(-${cardWidth * (cards.length - numVisibleCards)}px)`;
+              }
+          });
+      </script>
+    </main>
 </body>
-
 </html>
